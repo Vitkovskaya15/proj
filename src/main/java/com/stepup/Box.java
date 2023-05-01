1dced467bf0d8275da6e3c3a814d7c39c77764bb
Marina Koscheeva <ms.marina.vitkovskaya@mail.ru>
2023-05-01 19:18:05 +0300

    Изменена основная логика Добавлен класс коробки

---
+++ src/main/java/com/stepup/Box.java
@@ -0,0 +1,23 @@
+package com.stepup;
+
+public class Box {
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public Box(int size) {
+        this.size = size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+
+
+    }
+
+
+}
+
+
