d79c52a9f2b4b2807c15a5ac255f01daf02e10d6
Marina Koscheeva <ms.marina.vitkovskaya@mail.ru>
2023-05-01 19:38:13 +0300

    Улучшили логику

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,8 +4,8 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("1001");
-        System.out.println(2222);
-        System.out.println(3333);
+        System.out.println(7892);
+        System.out.println(66);
 
 
     }
@@ -13,8 +13,8 @@ public class MainApplication {
     public static void doSomething()
     {
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println(0);
+        System.out.println(0);
 
     }
 }
